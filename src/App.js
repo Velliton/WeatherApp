d1607f01ae322ba5b7ebe1dce4df57bb1c2e11da
Velliton <evg92faut@gmail.com>
2023-01-14 20:08:27 +0700

    weather-details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 /* import logo from './logo.svg'; */
+import sun from './assets/sun.png'
 import './App.css';
 
 function App() {
@@ -9,6 +10,29 @@ function App() {
           <i className='fa-solid fa-location-dot'></i>
           <input type="text" placeholder='Enter your location'></input>
           <button className='fa-solid fa-magnifying-glass'></button>
+        </div>
+        <div className='weather-box'>
+          <img src={sun}></img>
+          <p className='temperature'>22 
+            <span>°C</span>
+          </p>
+          <p className='description'>Clear sky</p>
+        </div>
+        <div className='weather-details'>
+          <div className='humidity'>
+            <i className='fa-solid fa-water'></i>
+            <div className='text'>
+              <span>50%</span>
+              <p>Humidity</p>
+            </div>
+          </div>
+          <div className='wind'>
+            <i className='fa-solid fa-wind'></i>
+            <div className='text'>
+              <span>5km\h</span>
+              <p>Wind Speed</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
